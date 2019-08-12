b9b92a1637cbf409b51edf46fb17d7efad9f207e
Eugene <gish.ee18@gmail.com>
2019-08-12 13:45:06 +0300

    Отправка скрытых команд при использовании команды help владельцом бота

--- tarakania-rpg/commands/command_help.py
+++ tarakania-rpg/commands/command_help.py
@@ -6,7 +6,11 @@ from context import Context
 class Command(BaseCommand):
     async def run(self, ctx: Context, args: Arguments) -> CommandResult:
         if len(args) == 0:
-            commands = list(self.bot._handler.get_all_commands())
+            commands = list(
+                self.bot._handler.get_all_commands(
+                    with_hidden=ctx.author.id in self.bot.config["owners"]
+                )
+            )
             commands.sort(key=lambda c: c.name)
 
             lines = [f"{c.name:<12}: {c.short_help}" for c in commands]
