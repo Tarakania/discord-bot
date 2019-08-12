b36b88fea76a5635b30a41674931248254bd6f51
Eugene <gish.ee18@gmail.com>
2019-08-12 14:01:23 +0300

    Добавлена команда runas

---
+++ tarakania-rpg/commands/administration/command_runas.py
@@ -0,0 +1,16 @@
+import copy
+
+from command import BaseCommand, CommandResult
+from parser.arguments import Arguments
+from context import Context
+
+
+class Command(BaseCommand):
+    async def run(self, ctx: Context, args: Arguments) -> CommandResult:
+        message = copy.copy(ctx.message)
+        message.author = args[0]
+        message.content = f"{ctx.prefix}{args[1].name} {' '.join(args[2:])}"
+
+        await self.bot._handler.process_message(message)
+
+        return f"Команда **{args[1].name}** обработана от имени **{args[0]}**"
