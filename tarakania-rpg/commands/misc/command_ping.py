31c81d59631fe059c589b8da8fc04c5a9ae9391e
Eugene <gish.ee18@gmail.com>
2019-08-12 15:24:58 +0300

    Исправлено неверное вычисление total diff

--- tarakania-rpg/commands/misc/command_ping.py
+++ tarakania-rpg/commands/misc/command_ping.py
@@ -19,15 +19,16 @@ class Command(BaseCommand):
         send_diff = request_complete_time - receive_time
 
         total_diff = (
-            ctx.message.edited_at or ctx.message.created_at
-        ) - msg.created_at
+            msg.created_at.timestamp()
+            - (ctx.message.edited_at or ctx.message.created_at).timestamp()
+        )
 
         return await msg.edit(
             content=(
                 f"```\n"
                 f"Receive diff: {round(receive_diff / 1000)}ms\n"
                 f"Message send: {round(send_diff * 1000)}ms\n"
-                f"Total diff:   {round(total_diff.microseconds / 1000)}ms\n"
+                f"Total diff:   {round(total_diff * 1000)}ms\n"
                 f"WS latency:   {round(self.bot.latency * 1000)}ms\n"
                 f"```"
             )
