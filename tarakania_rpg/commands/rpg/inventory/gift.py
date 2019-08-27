21d91ada689c63ed7e3337635db0cf5bceadfde8
MKokyn <max633925@gmail.com>
2019-08-27 21:55:06 +1000

    Добавлена возможность настройки количества предметов

--- tarakania_rpg/commands/rpg/inventory/gift.py
+++ tarakania_rpg/commands/rpg/inventory/gift.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 from handler import Context, Arguments, CommandResult
 from utils.confirmations import request_confirmation
 from utils.command_helpers import get_author_player
@@ -8,6 +10,15 @@ async def run(ctx: Context, args: Arguments) -> CommandResult:
 
     item = args[0]
     player2 = args[1]
+
+    if len(args) == 2:
+        number_items = 1
+    else:
+        if isinstance(args[2], str):
+            if args[2] == "all":
+                number_items = int(Counter(player.inventory)[item])
+        else:
+            number_items = args[2]
 
     if player == player2:
         return "Нельзя дарить вещи себе"
@@ -21,7 +32,7 @@ async def run(ctx: Context, args: Arguments) -> CommandResult:
         return f"В вашем инвентаре и экипировке нет **{item}**"
 
     confirmation_request = await ctx.send(
-        f"Вы действительно хотите передать **{item}** персонажу **{player2}**?"
+        f"Вы действительно хотите передать **{item}** в количестве **{str(number_items)}** персонажу **{player2}**?"
     )
     confirmation = await request_confirmation(ctx, confirmation_request)
 
@@ -30,12 +41,13 @@ async def run(ctx: Context, args: Arguments) -> CommandResult:
             content="Вы не подтвердили передачу предемета"
         )
 
-    await player.remove_item(item, ctx.bot.pg)
-    await player2.add_item(item, ctx.bot.pg)
+    for i in range(int(number_items)):
+        await player.remove_item(item, ctx.bot.pg)
+        await player2.add_item(item, ctx.bot.pg)
 
     return await confirmation_request.edit(
         content=(
-            f"**{player}** передал **{item}** **{player2}**"
+            f"**{player}** передал **{item}** x **{str(number_items)}** **{player2}**"
             f"{' из экипировки' if from_equipment else ''}"
         )
     )
